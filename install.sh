937f436da1b5926ee6f99582bdf080b42bd4cdce
addateve-cyber <addateve@gmail.com>
2026-07-21 15:59:19 +0500

    Add initial Minecraft VPS installer script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+clear
+echo "================================="
+echo "        SBHOST"
+echo "Minecraft VPS Installer"
+echo "================================="
+
+echo "Welcome to SBHOST!"
